b6544ad194c836f9cff6d2f6a6c96c776aa62572
Haris Khan <hk@hariskhan.dev>
2024-10-18 12:16:36 -0400

    expanded logging in destination.go

--- lib/common/destination/destination.go
+++ lib/common/destination/destination.go
@@ -2,6 +2,8 @@
 package destination
 
 import (
+	"github.com/go-i2p/go-i2p/lib/util/logger"
+	"github.com/sirupsen/logrus"
 	"strings"
 
 	. "github.com/go-i2p/go-i2p/lib/common/keys_and_cert"
@@ -10,6 +12,8 @@ import (
 	"github.com/go-i2p/go-i2p/lib/common/base64"
 	"github.com/go-i2p/go-i2p/lib/crypto"
 )
+
+var log = logger.GetLogger()
 
 /*
 [Destination]
@@ -31,26 +35,50 @@ type Destination struct {
 
 // Base32Address returns the I2P base32 address for this Destination.
 func (destination Destination) Base32Address() (str string) {
+	log.Debug("Generating Base32 address for Destination")
+
 	dest := destination.KeysAndCert.KeyCertificate.Bytes()
 	hash := crypto.SHA256(dest)
 	str = strings.Trim(base32.EncodeToString(hash[:]), "=")
 	str = str + ".b32.i2p"
+
+	log.WithFields(logrus.Fields{
+		"base32_address": str,
+	}).Debug("Generated Base32 address for Destination")
+
 	return
 }
 
 // Base64 returns the I2P base64 address for this Destination.
 func (destination Destination) Base64() string {
+	log.Debug("Generating Base64 address for Destination")
+
 	dest := destination.KeysAndCert.KeyCertificate.Bytes()
-	return base64.EncodeToString(dest)
+	base64Address := base64.EncodeToString(dest)
+
+	log.WithFields(logrus.Fields{
+		"base64_address_length": len(base64Address),
+	}).Debug("Generated Base64 address for Destination")
+
+	return base64Address
 }
 
 // ReadDestination returns Destination from a []byte.
 // The remaining bytes after the specified length are also returned.
 // Returns a list of errors that occurred during parsing.
 func ReadDestination(data []byte) (destination Destination, remainder []byte, err error) {
+	log.WithFields(logrus.Fields{
+		"input_length": len(data),
+	}).Debug("Reading Destination from bytes")
+
 	keys_and_cert, remainder, err := ReadKeysAndCert(data)
 	destination = Destination{
 		keys_and_cert,
 	}
+
+	log.WithFields(logrus.Fields{
+		"remainder_length": len(remainder),
+	}).Debug("Successfully read Destination from bytes")
+
 	return
 }
