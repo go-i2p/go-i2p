1d1d8126c22ccba2105ca15c4c2d760be4f2d4eb
Haris Khan <hk@hariskhan.dev>
2024-10-18 11:53:01 -0400

    adjusted logging in main.go

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"flag"
-
 	"github.com/go-i2p/go-i2p/lib/config"
 	"github.com/go-i2p/go-i2p/lib/router"
+	"github.com/go-i2p/go-i2p/lib/util/logger"
 	"github.com/go-i2p/go-i2p/lib/util/signals"
-	log "github.com/sirupsen/logrus"
 )
 
+var log = logger.GetLogger()
+
 func main() {
+
 	netDbPath := flag.String("netDb", config.DefaultNetDbConfig.Path, "Path to the netDb")
 	flag.Parse()
 	config.RouterConfigProperties.NetDb.Path = *netDbPath
 	go signals.Handle()
-	log.Info("parsing i2p router configuration")
-	log.Info("using netDb in:", config.RouterConfigProperties.NetDb.Path)
-	log.Info("starting up i2p router")
+	log.Debug("parsing i2p router configuration")
+	log.Debug("using netDb in:", config.RouterConfigProperties.NetDb.Path)
+	log.Debug("starting up i2p router")
 	r, err := router.CreateRouter()
 	if err == nil {
 		signals.RegisterReloadHandler(func() {
