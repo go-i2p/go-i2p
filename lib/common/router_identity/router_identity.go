cbe4af84594f5688c3e6d04ea2b0c68575b8841c
Haris Khan <hk@hariskhan.dev>
2024-11-17 21:44:32 -0500

    added AsDestination()

--- lib/common/router_identity/router_identity.go
+++ lib/common/router_identity/router_identity.go
@@ -3,6 +3,7 @@ package router_identity
 
 import (
 	"github.com/go-i2p/go-i2p/lib/common/certificate"
+	"github.com/go-i2p/go-i2p/lib/common/destination"
 	"github.com/go-i2p/go-i2p/lib/common/key_certificate"
 	. "github.com/go-i2p/go-i2p/lib/common/keys_and_cert"
 	"github.com/go-i2p/go-i2p/lib/crypto"
@@ -76,4 +77,10 @@ func NewRouterIdentity(publicKey crypto.PublicKey, signingPublicKey crypto.Signi
 	}).Debug("Successfully created RouterIdentity")
 
 	return &routerIdentity, nil
+}
+
+func (router_identity *RouterIdentity) AsDestination() destination.Destination {
+	return destination.Destination{
+		KeysAndCert: router_identity.KeysAndCert,
+	}
 }
