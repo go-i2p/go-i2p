da875738e471aa5f5e13e06101b7a9a5228ab854
Haris Khan <hk@hariskhan.dev>
2024-11-28 22:46:48 -0500

    fix typos

--- doc/tests/lease_set.mk
+++ doc/tests/lease_set.mk
@@ -4,19 +4,19 @@ test-lease-set-all: test-lease-set-creation \
                     test-lease-set-expirations \
                     test-lease-set-signature-verification
 test-lease-set-creation:
-	$(GO) test -v ./lease_set -run ^TestLeaseSetCreation$
+	$(GO) test -v ./lib/common/lease_set -run TestLeaseSetCreation
 
 test-lease-set-validation:
-	$(GO) test -v ./lease_set -run ^TestLeaseSetValidation$
+	$(GO) test -v ./lib/common/lease_set -run TestLeaseSetValidation
 
 test-lease-set-components:
-	$(GO) test -v ./lease_set -run ^TestLeaseSetComponents$
+	$(GO) test -v ./lib/common/lease_set -run TestLeaseSetComponents
 
 test-lease-set-expirations:
-	$(GO) test -v ./lease_set -run ^TestExpirations$
+	$(GO) test -v ./lib/common/lease_set -run TestExpirations
 
 test-lease-set-signature-verification:
-	$(GO) test -v ./lease_set -run ^TestSignatureVerification$
+	$(GO) test -v ./lib/common/lease_set -run TestSignatureVerification
 
 .PHONY: test-lease-set-all \
         test-lease-set-creation \
