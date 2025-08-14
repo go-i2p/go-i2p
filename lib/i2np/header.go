322b431e8ffbab2d502e4bc1532bc16193eb6d45
eyedeekay <hankhill19580@gmail.com>
2025-08-14 16:34:01 -0400

    move constants to own file

--- lib/i2np/header.go
+++ lib/i2np/header.go
@@ -61,21 +61,6 @@ data ::
         purpose -> actual message contents
 */
 
-const (
-	I2NP_MESSAGE_TYPE_DATABASE_STORE              = 1
-	I2NP_MESSAGE_TYPE_DATABASE_LOOKUP             = 2
-	I2NP_MESSAGE_TYPE_DATABASE_SEARCH_REPLY       = 3
-	I2NP_MESSAGE_TYPE_DELIVERY_STATUS             = 10
-	I2NP_MESSAGE_TYPE_GARLIC                      = 11
-	I2NP_MESSAGE_TYPE_TUNNEL_DATA                 = 18
-	I2NP_MESSAGE_TYPE_TUNNEL_GATEWAY              = 19
-	I2NP_MESSAGE_TYPE_DATA                        = 20
-	I2NP_MESSAGE_TYPE_TUNNEL_BUILD                = 21
-	I2NP_MESSAGE_TYPE_TUNNEL_BUILD_REPLY          = 22
-	I2NP_MESSAGE_TYPE_VARIABLE_TUNNEL_BUILD       = 23
-	I2NP_MESSAGE_TYPE_VARIABLE_TUNNEL_BUILD_REPLY = 24
-)
-
 type I2NPNTCPHeader struct {
 	Type       int
 	MessageID  int
