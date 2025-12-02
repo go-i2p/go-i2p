68743d22e6374d15e761e234157d64c5005fd73b
eyedeekay <hankhill19580@gmail.com>
2025-12-02 00:17:08 -0500

    Remove commented-out code in main function for cleaner implementation

--- main.go
+++ main.go
@@ -209,29 +209,4 @@ func main() {
 		debugPrintConfig()
 		os.Exit(1)
 	}
-	/*
-		netDbPath := flag.String("netDb", config.DefaultNetDbConfig.Path, "Path to the netDb")
-		flag.Parse()
-		config.RouterConfigProperties.NetDb.Path = *netDbPath
-		go signals.Handle()
-		log.Debug("parsing i2p router configuration")
-		log.Debug("using netDb in:", config.RouterConfigProperties.NetDb.Path)
-		log.Debug("starting up i2p router")
-		r, err := router.CreateRouter()
-		if err == nil {
-			signals.RegisterReloadHandler(func() {
-				// TODO: reload config
-			})
-			signals.RegisterInterruptHandler(func() {
-				// TODO: graceful shutdown
-				r.Stop()
-			})
-			r.Start()
-			r.Wait()
-			r.Close()
-		} else {
-			log.Errorf("failed to create i2p router: %s", err)
-		}
-
-	*/
 }
