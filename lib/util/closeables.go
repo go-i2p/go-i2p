4303e4818e37e4247b4c2c300e98d97bbb5c52f9
eyedeekay <hankhill19580@gmail.com>
2025-12-05 15:58:52 -0500

    Fix: Enhance thread safety in RegisterCloser and CloseAll functions

--- lib/util/closeables.go
+++ lib/util/closeables.go
@@ -1,14 +1,29 @@
 package util
 
-import "io"
+import (
+	"io"
+	"sync"
+)
 
-var closeOnExit []io.Closer
+var (
+	closeOnExit []io.Closer
+	closeMutex  sync.Mutex
+)
 
+// RegisterCloser registers an io.Closer to be closed during shutdown.
+// This function is thread-safe.
 func RegisterCloser(c io.Closer) {
+	closeMutex.Lock()
+	defer closeMutex.Unlock()
 	closeOnExit = append(closeOnExit, c)
 }
 
+// CloseAll closes all registered io.Closer instances and clears the list.
+// This function is thread-safe.
 func CloseAll() {
+	closeMutex.Lock()
+	defer closeMutex.Unlock()
+
 	for idx := range closeOnExit {
 		closeOnExit[idx].Close()
 	}
