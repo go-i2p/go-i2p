c85f4c2af266cde78c6d0d347d811a9dac3ad00f
eyedeekay <hankhill19580@gmail.com>
2025-12-10 11:32:32 -0500

    Remove hardcoded DEBUG_I2P value and set it conditionally in run script

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
+if [ -z "$DEBUG_I2P" ]; then
+    export DEBUG_I2P=warn
+fi
 rm -f go-i2p.log ~/Downloads/i2preseed.zip
 go clean -cache
 go build -v .
 wget -O ~/Downloads/i2preseed.zip http://localhost:7657/createreseed
-DEBUG_I2P=warn ./go-i2p --bootstrap.type file --bootstrap.reseed-file ~/Downloads/i2preseed.zip --i2cp.address localhost:8654 2>&1 | tee go-i2p.log
+./go-i2p --bootstrap.type file --bootstrap.reseed-file ~/Downloads/i2preseed.zip --i2cp.address localhost:8654 2>&1 | tee go-i2p.log
