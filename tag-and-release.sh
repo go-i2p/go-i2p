b8a6db75d7ece4918eddb62c39029d2d1007f110
eyedeekay <hankhill19580@gmail.com>
2026-01-27 16:30:05 -0500

    Add conditional check for RELEASE_NOTES.md in tagandrelease function

--- tag-and-release.sh
+++ tag-and-release.sh
@@ -33,16 +33,16 @@ collecthash() {
   echo "$1 tag hash: $TAG_HASH" 1>&2
   echo "Remote: $REMOTE" 1>&2
 }
-LOGGER_TAG_HASH=$(collecthash logger)
-CRYPTO_TAG_HASH=$(collecthash crypto)
-COMMON_TAG_HASH=$(collecthash common)
-NOISE_TAG_HASH=$(collecthash noise)
-GO_NOISE_TAG_HASH=$(collecthash go-noise)
-GO_I2P_TAG_HASH=$(collecthash go-i2p)
-GO_I2CP_TAG_HASH=$(collecthash go-i2cp)
-GO_DATAGRAMS_TAG_HASH=$(collecthash go-datagrams)
-GO_STREAMING_TAG_HASH=$(collecthash go-streaming)
-GO_SAM_BRIDGE_TAG_HASH=$(collecthash go-sam-bridge)
+LOGGER_TAG_HASH=$(collecthash logger) # 0
+CRYPTO_TAG_HASH=$(collecthash crypto) # 1
+COMMON_TAG_HASH=$(collecthash common) # 2
+NOISE_TAG_HASH=$(collecthash noise) # 3
+GO_NOISE_TAG_HASH=$(collecthash go-noise) # 4
+GO_I2P_TAG_HASH=$(collecthash go-i2p) # 5
+GO_I2CP_TAG_HASH=$(collecthash go-i2cp) # 6
+GO_DATAGRAMS_TAG_HASH=$(collecthash go-datagrams) # 7
+GO_STREAMING_TAG_HASH=$(collecthash go-streaming) # 8
+GO_SAM_BRIDGE_TAG_HASH=$(collecthash go-sam-bridge) # 9
 
 echo "Collected tag hashes. Proceeding to tag version v$VERSION" 1>&2
 
@@ -79,26 +79,28 @@ tagandrelease() {
   TAG_HASH=$(git rev-parse "v$VERSION")
   echo "$1 v$VERSION tag hash: $TAG_HASH" 1>&2
   echo "$TAG_HASH"
-  github-release release \
-  --user go-i2p \
-  --repo "$1" \
-  --tag "v$VERSION" \
-  --name "go-i2p v$VERSION" \
-  --description "$(cat RELEASE_NOTES.md)" 1>&2
+  if [ -f RELEASE_NOTES.md ]; then
+    github-release release \
+      --user go-i2p \
+      --repo "$1" \
+      --tag "v$VERSION" \
+      --name "go-i2p v$VERSION" \
+      --description "$(cat RELEASE_NOTES.md)" 1>&2
+  fi
   push
 }
 
 echo "Tagging and releasing version v$VERSION" 1>&2
 
-LOGGER_TAG_HASH=$(tagandrelease logger)
-CRYPTO_TAG_HASH=$(tagandrelease crypto)
-COMMON_TAG_HASH=$(tagandrelease common)
-NOISE_TAG_HASH=$(tagandrelease noise)
-GO_NOISE_TAG_HASH=$(tagandrelease go-noise)
-GO_I2P_TAG_HASH=$(tagandrelease go-i2p)
-GO_I2CP_TAG_HASH=$(tagandrelease go-i2cp)
-GO_DATAGRAMS_TAG_HASH=$(tagandrelease go-datagrams)
-GO_STREAMING_TAG_HASH=$(tagandrelease go-streaming)
-GO_SAM_BRIDGE_TAG_HASH=$(tagandrelease go-sam-bridge)
+LOGGER_TAG_HASH=$(tagandrelease logger) # 0
+CRYPTO_TAG_HASH=$(tagandrelease crypto) # 1
+COMMON_TAG_HASH=$(tagandrelease common) # 2
+NOISE_TAG_HASH=$(tagandrelease noise) # 3
+GO_NOISE_TAG_HASH=$(tagandrelease go-noise) # 4
+GO_I2P_TAG_HASH=$(tagandrelease go-i2p) # 5
+GO_I2CP_TAG_HASH=$(tagandrelease go-i2cp) # 6
+GO_DATAGRAMS_TAG_HASH=$(tagandrelease go-datagrams) # 7
+GO_STREAMING_TAG_HASH=$(tagandrelease go-streaming) # 8
+GO_SAM_BRIDGE_TAG_HASH=$(tagandrelease go-sam-bridge) # 9
 
 echo "Successfully tagged and released version v$VERSION" 1>&2
