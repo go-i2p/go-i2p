29bc997fd1fe213f4f764acac4bace008b5bd663
eyedeekay <hankhill19580@gmail.com>
2026-04-10 17:05:23 -0400

    refactor: replace "oops" package with standard errors in error constants

--- lib/transport/errors.go
+++ lib/transport/errors.go
@@ -1,10 +1,10 @@
 package transport
 
-import "github.com/samber/oops"
+import "errors"
 
 // error for when we have no transports available to use
-var ErrNoTransportAvailable = oops.Errorf("no transports available")
+var ErrNoTransportAvailable = errors.New("no transports available")
 
 // ErrConnectionPoolFull is returned when a connection pool has reached its
 // maximum capacity and cannot accept new connections.
-var ErrConnectionPoolFull = oops.Errorf("connection pool full")
+var ErrConnectionPoolFull = errors.New("connection pool full")
