1ae1c91139c8d529e44370fff2029455bb7b826e
eyedeekay <hankhill19580@gmail.com>
2026-07-06 14:34:37 -0400

    test(transport): add tests for attachDateTimeMetadataIfDue functionality

--- lib/transport/ntcp2/keepalive_test.go
+++ lib/transport/ntcp2/keepalive_test.go
@@ -1,6 +1,7 @@
 package ntcp2
 
 import (
+	"bytes"
 	"testing"
 	"time"
 
@@ -19,4 +20,29 @@ func TestNextMetaIntervalWithRand_JavaI2PRange(t *testing.T) {
 	mid := nextMetaIntervalWithRand(func(n int64) int64 { return n / 2 })
 	require.GreaterOrEqual(t, mid, half)
 	require.Less(t, mid, ntcp2MetaFrequency)
+}
+
+func TestAttachDateTimeMetadataIfDue_PrependsDateTimeBlock(t *testing.T) {
+	session := &NTCP2Session{}
+	framedData := []byte{0xAA, 0xBB, 0xCC}
+	metaPrefix := SerializeBlocks(NewDateTimeBlock())
+	nextMetaAt := time.Now().Add(-1 * time.Second)
+
+	result := session.attachDateTimeMetadataIfDue(framedData, &nextMetaAt)
+
+	require.GreaterOrEqual(t, nextMetaAt, time.Now().Add(-1*time.Second))
+	require.True(t, bytes.HasPrefix(result, metaPrefix))
+	require.Equal(t, framedData, result[len(metaPrefix):])
+}
+
+func TestAttachDateTimeMetadataIfDue_NotDueLeavesFrameUnchanged(t *testing.T) {
+	session := &NTCP2Session{}
+	framedData := []byte{0x11, 0x22, 0x33}
+	nextMetaAt := time.Now().Add(10 * time.Minute)
+	originalNextMetaAt := nextMetaAt
+
+	result := session.attachDateTimeMetadataIfDue(framedData, &nextMetaAt)
+
+	require.Equal(t, framedData, result)
+	require.Equal(t, originalNextMetaAt, nextMetaAt)
 }
